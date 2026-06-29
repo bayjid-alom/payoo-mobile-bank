3f7352909620cc26c0c24ee046e17899841dabf0
bayjidalom <bayjidalom1005@gmail.com>
2026-06-29 08:32:28 +0600

    feat: implement login functionality

--- - Notes/README.md
+++ - Notes/README.md
@@ -2,7 +2,45 @@
 - number input -> maxlength="11"
 - password input -> maxlength="4" 
 
+### when press login button
+```
+- 01. get the mobile number
+- 02. get the pin input
+- 03. match pin & mobile number
+- 03-01. true::::>> alert> homepage
 
+- window.location.replace("/home.html") - 
+- window.location.assign("/home.html")
+
+- 03-02. false:::>> alert> return
+
+```
+
+## replace() VS assing()
+- window.location.assign("/home.html")
+- নতুন page-এ যায়।
+- History-তে আগের page থাকে।
+- ✅ Back করলে আগের page-এ ফেরা যায়।
+
+<br>
+
+- window.location.replace("/home.html")
+- নতুন page-এ যায়।
+- History থেকে আগের page মুছে যায়।
+- ❌ Back করলে আগের page-এ ফেরা যায় না।
+
+
+
+
+
+
+
+
+
+
+
+ <br><br>
+---
 # 📝 Notes
 
 ### DaisyUI + tailwindCSS connect
---
+++ script/login.js
@@ -0,0 +1,27 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+    // 01. get the mobile number
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+
+
+    // 02. get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin)
+
+
+    // 03. match pin & mobile number
+    if (contactNumber == "01234567890" && pin == "1234") {
+        // 03-01. true::::>> alert> homepage
+        alert("Login successful");
+        // window.location.replace("/home.html")
+        window.location.assign("/home.html")
+    }
+
+    else {
+        // 03-02. false:::>> alert> return
+        alert("Login failed!");
+        return;
+    }
+
+})
